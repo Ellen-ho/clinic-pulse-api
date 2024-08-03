05c60fdfa77e3e420a03d9af8c21df1db0e13543
Ellen <ellen31725@gmail.com>
2024-08-03 16:58:27 +0800

    feat: add passport configuration and add findById method to user repository

---
+++ src/domain/user/interfaces/repositories/IUserRepository.ts
@@ -0,0 +1,7 @@
+import { IBaseRepository } from 'domain/shared/IBaseRepository'
+import { User } from '../../User'
+
+export interface IUserRepository extends IBaseRepository<User> {
+  findById: (id: string) => Promise<User | null>
+  findByEmail: (email: string) => Promise<User | null>
+}
