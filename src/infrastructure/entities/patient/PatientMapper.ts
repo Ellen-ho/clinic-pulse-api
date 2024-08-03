d0a86520dfa426e8ca2d82566305e5d10527e548
Ellen <ellen31725@gmail.com>
2024-08-03 20:32:17 +0800

    feat: add patient entity, mapper and domain model

---
+++ src/infrastructure/entities/patient/PatientMapper.ts
@@ -0,0 +1,31 @@
+import { IEntityMapper } from 'domain/shared/IEntityMapper'
+import { PatientEntity } from './PatientEntity'
+import { Patient } from 'domain/patient/Patient'
+
+export class PatientMapper implements IEntityMapper<PatientEntity, Patient> {
+  public toDomainModel(entity: PatientEntity): Patient {
+    const patient = new Patient({
+      id: entity.id,
+      firstName: entity.firstName,
+      lastName: entity.lastName,
+      birthDate: entity.birthDate,
+      gender: entity.gender,
+      createdAt: entity.createdAt,
+      updatedAt: entity.updatedAt,
+    })
+    return patient
+  }
+
+  public toPersistence(domainModel: Patient): PatientEntity {
+    const patientEntity = new PatientEntity()
+    patientEntity.id = domainModel.id
+    patientEntity.firstName = domainModel.firstName
+    patientEntity.lastName = domainModel.lastName
+    patientEntity.birthDate = domainModel.birthDate
+    patientEntity.gender = domainModel.gender
+    patientEntity.createdAt = domainModel.createdAt
+    patientEntity.updatedAt = domainModel.updatedAt
+
+    return patientEntity
+  }
+}
