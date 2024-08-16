154ed4147e4c5bfa1501cfb50e2f44f177822f47
Ellen <ellen31725@gmail.com>
2024-08-16 11:09:45 +0800

    feat: add update use cases to medicine treatment controller

--- src/infrastructure/http/controllers/MedicineTreatmentController.ts
+++ src/infrastructure/http/controllers/MedicineTreatmentController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express'
 import { UpdateConsultationToMedicineUseCase } from 'application/consultation/UpdateConsultationToMedicineUseCase'
 import { CreateMedicineTreatmentUseCase } from 'application/treatment/CreateMedicineTreatmentUseCase'
+import { UpdateMedicineTreatmentUseCase } from 'application/treatment/UpdateMedicineTreatmentUseCase'
+import { UpdateConsultationCheckOutAtUseCase } from 'application/consultation/UpdateConsultationCheckOutAtUseCase'
 
 export interface IMedicineTreatmentController {
   createMedicineTreatment: (req: Request, res: Response) => Promise<Response>
+  updateMedicineTreatment: (req: Request, res: Response) => Promise<Response>
 }
 
 export class MedicineTreatmentController
@@ -11,7 +14,9 @@ export class MedicineTreatmentController
 {
   constructor(
     private readonly createMedicineTreatmentUseCase: CreateMedicineTreatmentUseCase,
-    private readonly updateConsultationToMedicineUseCase: UpdateConsultationToMedicineUseCase
+    private readonly updateConsultationToMedicineUseCase: UpdateConsultationToMedicineUseCase,
+    private readonly updateMedicineTreatmentUseCase: UpdateMedicineTreatmentUseCase,
+    private readonly updateConsultationCheckOutAtUseCase: UpdateConsultationCheckOutAtUseCase
   ) {}
 
   public createMedicineTreatment = async (
@@ -28,5 +33,22 @@ export class MedicineTreatmentController
     await this.updateConsultationToMedicineUseCase.execute(request)
 
     return res.status(200).json(medicineTreatment)
+  }
+
+  public updateMedicineTreatment = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    const medicineTreatmentRequest = {
+      id: req.params.id,
+    }
+    await this.updateMedicineTreatmentUseCase.execute(medicineTreatmentRequest)
+
+    const consultationRequest = {
+      id: '6a7815ff-6d51-4351-b765-28b68ce61843',
+    }
+    await this.updateConsultationCheckOutAtUseCase.execute(consultationRequest)
+
+    return res.status(200).json()
   }
 }
