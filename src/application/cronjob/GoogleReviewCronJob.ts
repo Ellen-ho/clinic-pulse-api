88f69fc2ceeffa650b1c3d1851ffc3722f376eab
Ellen <ellen31725@gmail.com>
2024-08-24 09:35:33 +0800

    feat: add google review cron job

---
+++ src/application/cronjob/GoogleReviewCronJob.ts
@@ -0,0 +1,34 @@
+import GoogleReviewService from 'infrastructure/network/GoogleReviewService'
+import { IScheduler } from 'infrastructure/network/Scheduler'
+import schedule from 'node-schedule'
+
+export interface IGoogleReviewCronJob {
+  init: () => Promise<void>
+}
+
+export class GoogleReviewCronJob implements IGoogleReviewCronJob {
+  constructor(
+    private readonly googleReviewService: GoogleReviewService,
+    private readonly scheduler: IScheduler
+  ) {}
+
+  public async init(): Promise<void> {
+    this.createGoogleReviewCronJob()
+  }
+
+  private createGoogleReviewCronJob(): void {
+    const rule = new schedule.RecurrenceRule()
+    rule.hour = 1
+    rule.minute = 0
+
+    const jobCallback = async (): Promise<void> => {
+      await this.googleReviewService.fetchNewGoogleReviews()
+    }
+
+    this.scheduler.createJob(
+      `fetchNewGoogleReviews ${new Date().toISOString()}`,
+      rule,
+      jobCallback
+    )
+  }
+}
