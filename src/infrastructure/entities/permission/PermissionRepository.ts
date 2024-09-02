963b3cd93bfe167e08bd470ebd86f582bc283a2d
Ellen <ellen31725@gmail.com>
2024-09-02 22:44:48 +0800

    fix: import RepositoryError path

--- src/infrastructure/entities/permission/PermissionRepository.ts
+++ src/infrastructure/entities/permission/PermissionRepository.ts
@@ -1,43 +1,43 @@
-import { BaseRepository } from "../../../infrastructure/database/BaseRepository";
-import { PermissionEntity } from "./PermissionEntity";
-import { Permission } from "../../../domain/permission/Permission";
-import { IPermissionRepository } from "../../../domain/permission/interfaces/repositories/IPermissionRepository";
-import { DataSource } from "typeorm";
-import { PermissionMapper } from "./PermissionMapper";
-import { UserRoleType } from "../../../domain/user/User";
-import { RepositoryError } from "infrastructure/error/RepositoryError";
+import { BaseRepository } from '../../../infrastructure/database/BaseRepository'
+import { PermissionEntity } from './PermissionEntity'
+import { Permission } from '../../../domain/permission/Permission'
+import { IPermissionRepository } from '../../../domain/permission/interfaces/repositories/IPermissionRepository'
+import { DataSource } from 'typeorm'
+import { PermissionMapper } from './PermissionMapper'
+import { UserRoleType } from '../../../domain/user/User'
+import { RepositoryError } from '../../../infrastructure/error/RepositoryError'
 
 export class PermissionRepository
   extends BaseRepository<PermissionEntity, Permission>
   implements IPermissionRepository
 {
   constructor(dataSource: DataSource) {
-    super(PermissionEntity, new PermissionMapper(), dataSource);
+    super(PermissionEntity, new PermissionMapper(), dataSource)
   }
 
   public async findByRole(role: UserRoleType): Promise<Permission | null> {
     try {
       const rawPermissions = await this.getQuery<
         Array<{
-          id: string;
-          role: string;
-          dashboard_read: boolean;
-          consultation_read: boolean;
-          feedback_survey_read: boolean;
-          online_review_read: boolean;
-          report_center_read: boolean;
-          time_slot_read: boolean;
-          staff_management_read: boolean;
-          staff_management_edit: boolean;
-          profile_read: boolean;
-          profile_edit: boolean;
-          created_at: Date;
-          updated_at: Date;
+          id: string
+          role: string
+          dashboard_read: boolean
+          consultation_read: boolean
+          feedback_survey_read: boolean
+          online_review_read: boolean
+          report_center_read: boolean
+          time_slot_read: boolean
+          staff_management_read: boolean
+          staff_management_edit: boolean
+          profile_read: boolean
+          profile_edit: boolean
+          created_at: Date
+          updated_at: Date
         }>
-      >(`SELECT * FROM permissions WHERE role = $1`, [role]);
+      >(`SELECT * FROM permissions WHERE role = $1`, [role])
 
       if (rawPermissions.length === 0) {
-        return null;
+        return null
       }
 
       const {
@@ -55,7 +55,7 @@ export class PermissionRepository
         profile_edit,
         created_at,
         updated_at,
-      } = rawPermissions[0];
+      } = rawPermissions[0]
 
       return {
         id,
@@ -72,12 +72,12 @@ export class PermissionRepository
         profileEdit: profile_edit,
         createdAt: created_at,
         updatedAt: updated_at,
-      } as Permission;
+      } as Permission
     } catch (e) {
       throw new RepositoryError(
-        "PermissionRepository findByRole error",
+        'PermissionRepository findByRole error',
         e as Error
-      );
+      )
     }
   }
 }
